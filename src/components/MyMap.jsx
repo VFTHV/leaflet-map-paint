88f553532952b31739b7f85462dc958dab81b945
Vadim Fattakhov <vadfthv@gmail.com>
2024-01-10 21:38:14 -0600

    Styled with onEachCountry

--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -1,14 +1,51 @@
 import React, { useEffect } from 'react';
-import countries from '../data/countries.json';
+import { MapContainer, GeoJSON } from 'react-leaflet';
+import mapData from '../data/countries.json';
+import 'leaflet/dist/leaflet.css';
+import './MyMap.css';
 
 export default function MyMap() {
-  useEffect(() => {
-    console.log(countries);
-  }, []);
+  const countryStyle = {
+    fillColor: 'green',
+    fillOpacity: 1,
+    color: 'black',
+    weight: 2,
+  };
+
+  const printMessageToConsole = (event) => {
+    console.log('clicked');
+  };
+  const changeCountryColor = (event) => {
+    console.log(event);
+    event.target.setStyle({ color: 'green', fillColor: 'yellow' });
+  };
+
+  const onEachCountry = (country, layer) => {
+    const countryName = country.properties.ADMIN;
+
+    layer.bindPopup(countryName);
+    layer.on({
+      click: changeCountryColor,
+      // mouseover: changeCountryColor,
+    });
+  };
 
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>My Map</h1>
+      <MapContainer
+        style={{
+          height: '80vh',
+        }}
+        zoom={2}
+        center={[29.646539, -95.673716]}
+      >
+        <GeoJSON
+          style={countryStyle}
+          data={mapData.features}
+          onEachFeature={onEachCountry}
+        />
+      </MapContainer>
     </div>
   );
 }
